e9cb88d7816846964efcb721c4e46bea9bb1d8a0
Elizaveta Melikhova <lizamelihovaa@gmail.com>
2024-03-17 19:51:44 +0200

    threading sort

--- sort.py
+++ sort.py
@@ -6,12 +6,11 @@ import shutil
 import logging
 import time
 
-logging.basicConfig(level=logging.DEBUG, format="%(threadName)s %(message)s")
+logging.basicConfig(level=logging.DEBUG, format="%(threadName)s")
 
 known_extensions = set()
 unknown_extensions = set()
 files = []
-folders = []
 
 
 def mapping():
@@ -92,9 +91,9 @@ def define_category(suffix):
 def move(root_path, file_path, category):
     suffix = file_path.suffix
     name = file_path.stem
-    new_name = normalize(name)+suffix
-    new_folder = root_path/category
-    new_path = new_folder/new_name
+    new_name = normalize(name) + suffix
+    new_folder = root_path / category
+    new_path = new_folder / new_name
 
     new_folder.mkdir(exist_ok=True)
     file_path.rename(new_path)
@@ -103,10 +102,12 @@ def move(root_path, file_path, category):
 def sort_folder(root_path, folder_path):
     for el in folder_path.iterdir():
         if el.is_dir():
-            sort_folder(root_path, el)
-            folders.append(el)
-        elif el.is_file():
-            files.append(el)
+            inner_thread = threading.Thread(target=sort_folder, args=(root_path, el))
+            inner_thread.start()
+            inner_thread.join()
+            remove_empty_folder(el)
+        else:
+            process_files(root_path, el)
 
 
 def remove_empty_folder(folder: Path):
@@ -115,7 +116,6 @@ def remove_empty_folder(folder: Path):
 
 
 def main():
-    start = time.time()
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--source', '-s')
@@ -124,27 +124,11 @@ def main():
 
     if source_folder:
         source_folder = Path(source_folder)
-        sort_folder(source_folder, source_folder)
-
-        threads = []
-        for file in files:
-            th = threading.Thread(target=process_files, args=(source_folder, file, ))
-            th.start()
-            threads.append(th)
-        [thread.join() for thread in threads]
-
-        folder_threads = []
-        for folder in folders:
-            th = threading.Thread(target=remove_empty_folder, args=(folder,))
-            th.start()
-            folder_threads.append(th)
-        [thread.join() for thread in folder_threads]
+        th = threading.Thread(target=sort_folder, args=(source_folder, source_folder))
+        th.start()
 
     else:
         print("Please provide a source folder.")
-
-    end = time.time()
-    print(end - start)
 
 
 if __name__ == '__main__':
